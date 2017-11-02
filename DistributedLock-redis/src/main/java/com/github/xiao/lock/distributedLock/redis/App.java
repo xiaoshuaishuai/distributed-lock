27c894f7003b205585532120a1c9b12fd9068a8e
shuaishuaixiao <xstojob@gamil.com>
2017-11-02 18:29:23 +0800

    分布式锁

---
+++ DistributedLock-redis/src/main/java/com/github/xiao/lock/distributedLock/redis/App.java
@@ -0,0 +1,13 @@
+package com.github.xiao.lock.distributedLock.redis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ DistributedLock-zookeeper/src/main/java/com/github/xiao/lock/distributedLock/zookeeper/App.java
@@ -0,0 +1,13 @@
+package com.github.xiao.lock.distributedLock.zookeeper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
